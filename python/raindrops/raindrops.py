205ff039e8bfa74a81d0471f440bedc1ae9e3dec
Maksim Panin <me@mpanin.me>
2021-08-06 21:47:35 +0300

    add solution for python raindrop

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,11 @@
-def convert(number):
-    pass
+def convert(number: int) -> str:
+    result_str = ""
+    if number % 3 == 0:
+        result_str = "Pling"
+    if number % 5== 0:
+        result_str += "Plang"
+    if number % 7== 0:
+        result_str += "Plong"
+    if result_str == "":
+        return str(number)
+    return result_str
